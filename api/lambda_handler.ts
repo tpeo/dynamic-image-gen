92716b293c113f262f84dc716403d4f64f681775
wongmac <jonathanwong@utexas.edu>
2021-04-11 00:05:43 -0500

    cleared up some stuff

--- api/lambda_handler.ts
+++ api/lambda_handler.ts
@@ -1,33 +1,23 @@
 import { getScreenshot } from './_lib/chromium_lambda';
 import { getHtml as instaStory } from './_templates/insta-story/template';
-// import { Context } from 'aws-lambda';
 
 exports.instaStory = async function (event: any = {}): Promise<any> {
     try {
-        console.log(event)
-        var html = '';
-        // let query = parsedReq.query;
+        if (event.queryStringParameters === null) throw '';
+        let query = event.queryStringParameters;
         var file = null;
         const fileType = 'jpeg';
         //determine image template to return
-        // const queryData = {
-        //     movement: (query.movement as string || 'Do Your Part'),
-        //     actions: (query.actions as string || 'completed 0 actions'),
-        //     slug: (query.slug as string || 'dyp')
-        // }
         const queryData = {
-            movement: 'Do Your Part',
-            actions: 'completed 0 actions',
-            slug: 'dyp'
+            movement: (query.movement as string || 'Do Your Part'),
+            actions: (query.actions as string || 'completed 0 actions'),
+            slug: (query.slug as string || 'dyp')
         }
-        html = instaStory(queryData);
-        console.log(html);
+        let html = instaStory(queryData);
         file = await getScreenshot(html, fileType, false, 1080, 1920);
-        console.log(file);
         let cacheType = `public, immutable, no-transform, s-maxage=0, max-age=0`;
         // let cacheType = `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`;
         //handle error
-        console.log('time to return');
         return {
             statusCode: 200,
             headers: {
@@ -48,6 +38,5 @@ exports.instaStory = async function (event: any = {}): Promise<any> {
             body: '<h1>Internal Error</h1><p>Sorry, there was a problem</p>',
           };
     }
-
 };
 
